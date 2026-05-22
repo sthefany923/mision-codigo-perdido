ddf03fab9743f6908fbfed6c57ce65f0fb97231c
Danilopro233 <1028leandro@gmail.com>
2026-05-22 15:16:00 -0600

    Hola

--- src/mision/poder.java
+++ src/mision/poder.java
@@ -4,7 +4,20 @@ public class poder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
+	    // Atributos del hechizo
+	    public String nombre;
+	    public int danio; 
 
-}
+	    // El constructor en minúsculas
+	    public poder(String nombre, int danio) {
+	        this.nombre = nombre;
+	        this.danio = danio;
+	    }
+
+	    // Encantamiento que retorna el mensaje épico
+	    public String activar() {
+	        return "¡El cielo se quiebra al liberar la " + this.nombre + " causando " + this.danio + " de daño!";
+	    }
+	}
+	
